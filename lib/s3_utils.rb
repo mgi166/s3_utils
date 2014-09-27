d436275428dd4b8cffd6c3fe859fa73b56e13545
mgi166 <skskoari@gmail.com>
2014-09-28 08:23:27 +0900

    * Upload the file if src is File, Upload the directory if src is Directory.

--- lib/s3_utils.rb
+++ lib/s3_utils.rb
@@ -3,9 +3,28 @@ require 'aws-sdk'
 module S3Utils
   def upload_to_s3(src, dest)
     d_path = Path.new(dest)
+    s_path = Path.new(src)
 
-    objects = bucket(d_path.bucket_name).objects[d_path.path_without_bucket]
-    objects.write(:file => src)
+    if s_path.file?
+      upload_path = if d_path.end_with?('/')
+                      File.join(d_path.path_without_bucket, s_path.basename)
+                    else
+                      File.join(d_path.path_without_bucket)
+                    end
+
+      objects = bucket(d_path.bucket_name).objects[upload_path]
+      objects.write(:file => src)
+    end
+
+    if s_path.directory?
+      upload_targets = Dir.glob(File.join(s_path, '*'))
+
+      upload_targets.each do |file|
+        upload_path = File.join(d_path.path_without_bucket, file.basename)
+        objects = bucket(d_path.bucket_name).objects[upload_path]
+        objects.write(:file => src)
+      end
+    end
   end
 
   private
--- lib/s3_utils/path.rb
+++ lib/s3_utils/path.rb
@@ -20,5 +20,21 @@ module S3Utils
     def element
       @element ||= @path.to_s.split(Pathname::SEPARATOR_PAT)
     end
+
+    def basename
+      @path.basename
+    end
+
+    def directory?
+      @path.directory?
+    end
+
+    def file?
+      @path.file?
+    end
+
+    def end_with?(suffix)
+      @path.to_s.end_with?(suffix)
+    end
   end
 end
