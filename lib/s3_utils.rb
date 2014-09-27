aa65abf6010540b9d839ea509e5ff23ef596e612
mgi166 <skskoari@gmail.com>
2014-09-27 15:36:20 +0900

    * Add new method #upload_to_s3    * src expect path of file    * dest expect path of directory

--- lib/s3_utils.rb
+++ lib/s3_utils.rb
@@ -1,5 +1,21 @@
-require "s3_utils/version"
+require 'aws-sdk'
 
 module S3Utils
-  # Your code goes here...
+  def upload_to_s3(src, dest)
+    bucket_name = dest.split('/').first
+
+    upload_path = File.join(dest.split('/').drop(1), File.basename(src))
+    object = bucket(bucket_name).objects[upload_path]
+    object.write(:file => src)
+  end
+
+  private
+
+  def s3
+    ::AWS::S3.new
+  end
+
+  def bucket(bucket_name)
+    s3.buckets[bucket_name]
+  end
 end
