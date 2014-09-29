3d8f4457daecf505b649e08c8c37129f47d9e15f
mgi166 <skskoari@gmail.com>
2014-09-29 09:23:54 +0900

    * Add new module Method and replace the methods to S3Utils::Method

---
+++ lib/s3_utils/method.rb
@@ -0,0 +1,42 @@
+module S3Utils
+  module Method
+    def upload_to_s3(src, dest)
+      d_path = Path.new(dest)
+      s_path = Path.new(src)
+
+      if s_path.file?
+        upload_path = if d_path.end_with?('/')
+                        File.join(d_path.path_without_bucket, s_path.basename)
+                      else
+                        File.join(d_path.path_without_bucket)
+                      end
+
+        objects = bucket(d_path.bucket_name).objects[upload_path]
+        objects.write(:file => src)
+      end
+
+      if s_path.directory?
+        upload_targets = Dir[File.join(s_path.to_s, '**', '*')].reject do |path|
+          File.directory?(path)
+        end
+
+        upload_targets.each do |file|
+          p = File.join(d_path.path_without_bucket, file)
+          upload_path = Pathname.new(p).cleanpath
+          objects = bucket(d_path.bucket_name).objects[upload_path]
+          objects.write(:file => file)
+        end
+      end
+    end
+
+    private
+
+    def s3
+      ::AWS::S3.new
+    end
+
+    def bucket(bucket_name)
+      s3.buckets[bucket_name]
+    end
+  end
+end
