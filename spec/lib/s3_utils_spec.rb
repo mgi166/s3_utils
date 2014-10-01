16b63ea438493c65b68b3f0e706430d324dbce21
mgi166 <skskoari@gmail.com>
2014-10-01 09:52:45 +0900

    * Add FakeS3::Server to `before` and `after`

---
+++ spec/lib/s3_utils_spec.rb
@@ -0,0 +1,33 @@
+require 'tmpdir'
+require 'fakes3/server'
+
+describe S3Utils do
+  before(:all) do
+    @pid = fork do
+      FakeS3::Server.new('0.0.0.0', 12345, FakeS3::FileStore.new('/var/tmp/fakes3'), 'localhost').serve
+    end
+  end
+
+  after(:all) do
+    Process.kill(:TERM, @pid) if @pid
+  end
+
+  describe '#upload_to_s3' do
+    before do
+      AWS.config(s3_endpoint: 'localhost', s3_force_path_style: true, s3_port: 12345, use_ssl: false)
+    end
+
+    context 'when source is file' do
+      subject(:upload_to_s3) do
+        klass = Class.new do
+          include S3Utils
+        end.new
+
+        klass.upload_to_s3('hoge', 'fuga')
+      end
+
+      it 'description' do
+      end
+    end
+  end
+end
